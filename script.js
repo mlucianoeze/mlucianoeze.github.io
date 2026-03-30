49eb0a0b86cbbbd56c82c7ec9092d19b2bbb914d
Luciano Marti <mlucianoeze@gmail.com>
2026-03-30 18:37:34 -0300

    Deltarune-like site

--- script.js
+++ script.js
@@ -1,19 +1,93 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('clickBtn');
     const clickMsg = document.getElementById('clickMsg');
     const img = document.querySelector('#imgContainer img');
     let clickCount = 0;
 
-    button.addEventListener('click', function() {
-        clickCount++;
-        if (clickCount === 1) {
-            clickMsg.textContent = 'Hiciste un clic.';
-        } else {
-            clickMsg.textContent = `Hiciste clic ${clickCount} veces.`;
-        }
+    const gone = document.getElementById('gone');
+    const ominous = document.getElementById('ominous');
 
+    let dialogEnabled = true;
+    const dialogBox = document.getElementById('dialog');
+    const dialogopts = document.querySelectorAll('#dialog .option');
+    dialogopts.forEach(option => {
+        const text = option.querySelector('p');
+        text.addEventListener('mouseover', function() {
+            updateSelected(option);
+        });
+    });
+
+    const proceed = document.getElementById('proceed');
+    const doNot = document.getElementById('do-not');
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'ArrowLeft') {
+            updateSelected(proceed);
+        }
+        if (event.key === 'ArrowRight') {
+            updateSelected(doNot);
+        }
+        if (event.key === 'Enter' || event.key === 'z') {
+            const selected = document.querySelector('.dialogbox .selected');
+            if (selected) {
+                selected.click();
+            }
+        }
+    });
+
+    proceed.addEventListener('click', function() {
+        updateSelected(proceed);
+        clicked();
+    });
+
+    const placeholder = document.querySelector('.dialogbox .placeholder');
+    doNot.addEventListener('click', function() {
+        updateSelected(doNot);
+        if (!dialogEnabled) {
+            return;
+        }
+        if (clickCount >= 10) {
+            img.classList.remove('visible');
+            gone.currentTime = 0;
+            gone.play();
+        }
+        clickCount = 0;
+        clickMsg.style = 'visibility: hidden;';
+        dialogBox.style = 'visibility: hidden;';
+        updateSelected(null);
+        dialogEnabled = false;
+        window.setTimeout(() => {
+            dialogEnabled = true;
+            dialogBox.style = 'visibility: visible;';
+            updateSelected(placeholder);
+        }, 500);
+    });
+
+    function clicked() {
+        if (!dialogEnabled) {
+            return;
+        }
+        clickCount++;
+        clickMsg.style = '';
+        clickMsg.textContent = `${clickCount}.`;
         if (clickCount >= 10) {
             img.classList.add('visible');
         }
-    });
+        if (clickCount === 10) {
+            ominous.currentTime = 0;
+            ominous.play();
+        }
+    }
+
+    function updateSelected(option) {
+        if (!dialogEnabled) {
+            return;
+        }
+        const dialogopts = document.querySelectorAll('.dialogbox > *');
+        dialogopts.forEach(option => {
+            option.classList.remove('selected');
+        });
+        if (option) {
+            option.classList.add('selected');
+        }
+    }
 });
